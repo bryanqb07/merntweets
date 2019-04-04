c0d2ae9f10c07f3665517e117b3f167129dcef70
Bryan Lynch <bml312@nyu.edu>
2019-04-04 17:52:34 -0400

    model route validation add

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 5000;
+const passport = require("passport");
 
 mongoose
     .connect(db, { useNewUrlParser: true })
@@ -9,7 +14,15 @@ mongoose
     .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello world"));
-const port = process.env.PORT || 5000;
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
